ef5b25f46071fff8e9bb1d8ff0ce4f802fd1a506
Paschal Ikenna <ikbros16@yahoo.com>
2022-01-20 11:27:11 +0100

    Thursday

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
 import React from "react";
+import axios, { Axios } from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  state = { images: [] };
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID 88Tf7pl3yDrj6P95llvwyY0IgHVuSV2DsQiIVKx2qpU",
+      },
+    });
+
+    this.setState({ images: response.data.results });
   }
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "100px" }}>
         {" "}
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} Images
       </div>
     );
   }
