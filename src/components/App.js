81ca46a037c7d04730d69d1f571e445268092c69
Paschal Ikenna <ikbros16@yahoo.com>
2022-02-20 10:47:08 +0000

    Testing Staging environment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import axios, { Axios } from "axios";
+// import axios, { Axios } from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   state = { images: [] };
   async onSearchSubmit(term) {
-    const response = await axios.get("https://api.unsplash.com/search/photos", {
+    const response = await get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization: "Client-ID 88Tf7pl3yDrj6P95llvwyY0IgHVuSV2DsQiIVKx2qpU",
